a50737b36b2778747a1355658223be60eeadb40a
Izzy Lancaster <avrisaac555@gmail.com>
2023-02-26 15:23:19 -0800

    Call Discord api to get an access token

--- init.lua
+++ init.lua
@@ -2,7 +2,9 @@
     name = "luvit-discord"
     version = "0.0.1"
     dependencies = {
-      "creationix/weblit"
+      "creationix/weblit",
+      "creationix/coro-http",
+      "luvit/secure-socket"
     }
     description = "Discord OAuth on luvit proof-of-concept"
     tags = { "luvit", "lit", "discord" }
@@ -12,7 +14,7 @@
   ]]
 
 -- Constants 
-local LISTEN_HOST = os.getenv("LUA_LISTEN_HOST") or "0.0.0.0"
+local LISTEN_HOST = os.getenv("LUA_LISTEN_HOST") or "localhost"
 local LISTEN_PORT = os.getenv("LUA_LISTEN_PORT") or "8080"
 local DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID") or error("DISCORD_CLIENT_ID environment variable required, but not set")
 local DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET") or error("DISCORD_CLIENT_SECRET environment variable required, but not set")
@@ -21,6 +23,7 @@ local BASE_URL = os.getenv("LUA_BASE_URL") or "http://localhost:8080"
 local html = require("html")
 local querystring = require("querystring")
 local useful = require("useful")
+local coro_request = require("coro-http").request
 
 local function generateDiscordAuthorizationUrl (scopes, state, redirect_uri)
   return "https://discord.com/api/oauth2/authorize?" .. querystring.stringify({
@@ -33,6 +36,28 @@ local function generateDiscordAuthorizationUrl (scopes, state, redirect_uri)
   })
 end
 
+--- Requests an access token from the discord API given an authorization code
+--- Must be run in a coroutine
+---@param code string
+local function requestDiscordAccessToken (code)
+  local discord_token_endpoint = "https://discord.com/api/oauth2/token"
+
+  local payload = querystring.stringify({
+    client_id = DISCORD_CLIENT_ID,
+    client_secret = DISCORD_CLIENT_SECRET,
+    grant_type = "authorization_code",
+    code = code,
+    redirect_uri = BASE_URL .. "/callback/discord_auth"
+  })
+
+  local headers = {
+    {"Content-Type", "application/x-www-form-urlencoded"},
+    {"Content-Length", #payload}
+  }
+
+  return coro_request("POST", discord_token_endpoint, headers, payload)
+end
+
 require("weblit-app")
     .bind({
       host = LISTEN_HOST,
@@ -42,11 +67,24 @@ require("weblit-app")
     .use(require("weblit-auto-headers"))
     .use(require("session"))
     .route({method="GET", path="/callback/discord_auth"}, function (req, res, go)
-      print(req)
-      print(res)
-      
+      local code = req.query.code
+      local state = req.query.state
+
+      if state ~= req.session.state then
+        res.code = 401
+        res.body = "State mismatch"
+        return
+      end
+
+      local accessTokenResponse, accessTokenBody = requestDiscordAccessToken (code)
+
+      if accessTokenResponse.code ~= 200 then
+        res.code = 500
+        res.body = "<h1>Error Requesting Discord Access Token</h1><code>" .. accessTokenBody .. "</code>"
+      end
+
       res.code = 200
-      res.body = "Callback recieved"
+      res.body = "<h1>Success!</h1><code>" .. accessTokenBody .. "</code>"
     end)
     .route({method="GET", path="/"}, function (req, res, go)
       req.session.state = useful.generateRandomString(useful.alphaNumericCharset, 8)
