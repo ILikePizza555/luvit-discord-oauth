362ca6e9cc571539ef6eb128b04e97848379050a
Izzy Lancaster <avrisaac555@gmail.com>
2023-02-26 15:22:39 -0800

    Using flake-utils to make flake easier to read

--- flake.nix
+++ flake.nix
@@ -2,28 +2,24 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
         luvit-nix.url = "github:aiverson/luvit-nix";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = {self, nixpkgs, luvit-nix}:
-    let
-        forEachSystem systems: callback: nixpkgs.lib.genAttrs systems (system:
-            callback system nixpkgs.legacyPackages.${system});
-        
-        defaultSystems = ["aarch64-darwin" "x86_64-linux"];
-    in
-    {
-        devShells = forEachSystem defaultSystems (system: pkgs: 
-        let
+    outputs = {self, nixpkgs, luvit-nix, flake-utils}:
+        flake-utils.lib.eachDefaultSystem (system:
+        let 
+            pkgs = import nixpkgs { inherit system; };
             luvitPkgs = luvit-nix.packages.${system};
         in
         {
-            default = pkgs.mkShell {
-                packages = [
-                    luvitPkgs.luvit
-                    luvitPkgs.luvi
-                    luvitPkgs.lit
-                ];
-            }
-        })
-    }
+            devShells = {
+                default = pkgs.mkShell {
+                    packages = [
+                        luvitPkgs.luvit
+                        luvitPkgs.luvi
+                        luvitPkgs.lit
+                    ];
+                };
+            };
+        });
 }
